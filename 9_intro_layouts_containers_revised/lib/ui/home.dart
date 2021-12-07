8764aa405bb4f01ea37d4999d25560f9cc02ef70
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-12-07 12:55:45 +0530

    part 2

--- 9_intro_layouts_containers_revised/lib/ui/home.dart
+++ 9_intro_layouts_containers_revised/lib/ui/home.dart
@@ -25,17 +25,17 @@ class Home extends StatelessWidget {
 
       // part 3 --------------------------------------
       // row
-//      child: new Row(
+//      child:  Row(
 //        mainAxisAlignment: MainAxisAlignment.center,
 //        children: <Widget>[
-//          new Text(
+//           Text(
 //            "Item 1",
 //            textDirection: TextDirection.ltr,
-//            style: new TextStyle(fontSize: 12.9),
+//            style:  TextStyle(fontSize: 12.9),
 //          ),
-//          new Text("Item 2",
+//           Text("Item 2",
 //              textDirection: TextDirection.ltr,
-//              style: new TextStyle(fontSize: 12.9)),
+//              style:  TextStyle(fontSize: 12.9)),
       // expand will take the only space which is left
 //          const Expanded(child: const Text("Item 3"))
 //        ],
@@ -43,43 +43,53 @@ class Home extends StatelessWidget {
 
       // part 2 -------------------------------------------------
       // column
-//       child: new Column(
-      // to make it in the center of the screen
-//          mainAxisAlignment: MainAxisAlignment.center,
-      // here because of the column which takes a lot of different children
-      //  we will have all the different nodes whihc inside have other widgets
-      //  children will contain the array of all the widgets
-//          children: <Widget>[
-//              new Text("First item" , textDirection: TextDirection.ltr,
-//                       style: new TextStyle(color: Colors.white),),
-//
-//              new Text("Second item" , textDirection: TextDirection.ltr,
-//                style: new TextStyle(color: Colors.blue),),
-//
-      // we can also put container here
-      // container can contain other layout widget
-//               new Container(
-      // shades of the colour
-//                   color: Colors.deepOrange.shade50,
-      // bottom left allignment
-//                   alignment: Alignment.bottomLeft,
-//
-//                     child:new Text("Third item" , textDirection: TextDirection.ltr,
-//                       style: new TextStyle(color: Colors.blue, fontSize: 10.1),),
-//               )
-//
-//          ],
-//       ),
+      child: Column(
+        // to make it in the center of the screen
+        mainAxisAlignment: MainAxisAlignment.center,
+        // here because of the column which takes a lot of different children
+        // we will have all the different nodes whihc inside have other widgets
+        // children will contain the array of all the widgets
+        children: <Widget>[
+          const Text(
+            "First item",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.white),
+          ),
 
-      // part 1 -----------------------------------------------------------------------
-      // ltr - left to right
-      // TextStyle chnaging the style of the text
-      child: const Text(
-        "Hello Container",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-            color: Colors.white, fontWeight: FontWeight.w900, fontSize: 18.3),
+          const Text(
+            "Second item",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.blue),
+          ),
+
+          // we can also put container here
+          // container can contain other layout widget
+          Container(
+            // shades of the colour
+            color: Colors.deepOrange.shade100,
+            // bottom left allignment
+            alignment: Alignment.bottomLeft,
+            child: const Text(
+              "Third item",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.blue, fontSize: 10.1),
+            ),
+          )
+        ],
       ),
     );
   }
 }
+
+//       // part 1 -----------------------------------------------------------------------
+//       // ltr - left to right
+//       // TextStyle chnaging the style of the text
+//       child: const Text(
+//         "Hello Container",
+//         textDirection: TextDirection.ltr,
+//         style: TextStyle(
+//             color: Colors.white, fontWeight: FontWeight.w900, fontSize: 18.3),
+//       ),
+//     );
+//   }
+// }
