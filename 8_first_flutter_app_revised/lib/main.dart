6f19a9f551ed80c7ba0e70e4d1f315a07720e809
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-12-07 10:51:20 +0530

    part 3 using welcome lib

--- 8_first_flutter_app_revised/lib/main.dart
+++ 8_first_flutter_app_revised/lib/main.dart
@@ -5,33 +5,33 @@ void main() {
   runApp(
       // here material is very generic
       // here using materialApp we are more specific
-      MaterialApp(
-          // Assigning name to the title
-          title: "Welcome App",
-          // here is what we want to show
-          // home: Welcome(),
-          // ));
-
-          // part 2----------------------------------
-
-          // to use a material design component
-          home: Container(
-              // setting the color of the entire view
-              // in the output the text is placed at the corner left of the screen
-              color: Colors.blue,
-              // now adding center will be helpfull in placement of the text
-              child: const Center(
-                  child: Text(
-                "hello Material!",
-                textDirection: TextDirection.ltr,
-                // increasing the font of the text
-                // changing the font sty;e to italic
-                style: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  fontStyle: FontStyle.italic,
-                ),
-              )))));
+      const MaterialApp(
+    // Assigning name to the title
+    title: "Welcome App",
+    // here is what we want to show
+    home: Welcome(),
+  ));
 }
+//           // part 2----------------------------------
+
+//           // to use a material design component
+//           home: Container(
+//               // setting the color of the entire view
+//               // in the output the text is placed at the corner left of the screen
+//               color: Colors.blue,
+//               // now adding center will be helpfull in placement of the text
+//               child: const Center(
+//                   child: Text(
+//                 "hello Material!",
+//                 textDirection: TextDirection.ltr,
+//                 // increasing the font of the text
+//                 // changing the font sty;e to italic
+//                 style: TextStyle(
+//                   fontWeight: FontWeight.w800,
+//                   fontStyle: FontStyle.italic,
+//                 ),
+//               )))));
+// }
           // // part 1 ---------------------------
 
           // // here we pass a contaier called center, which is going to put everything
