4353ac02be79e73ce9dea1a76c430065293e3603
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-12-07 12:35:04 +0530

    9 layout part 1

---
+++ 9_intro_layouts_containers_revised/android/app/src/main/kotlin/com/example/intro_layouts_containers_revised/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.intro_layouts_containers_revised
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 9_intro_layouts_containers_revised/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import './ui/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Layouts",
+    // it fills the entire view
+    home: Home(),
+  ));
+}
